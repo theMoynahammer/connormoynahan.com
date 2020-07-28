7ce995607076b4dfd0739ab6ad7c2ecd621b3ebc
Connor Moynahan <connormoynahan@julias-mbp.lan>
2020-07-28 17:06:52 -0400

    Initial dist subtree commit

--- src/text/aboutThisSite.js
+++
@@ -1,7 +0,0 @@
-export default {
-    text: `ConnorMoynahan.com is a single page application made with Vue.js, 
-    the Vuetify UI library, with the base outline taken from w3schools.com. 
-    It is hosted on github pages; which is a free, TLS-enabled hosting service. 
-    The goal was to make a simple and easy to navigate site to display my resume, motivations, and information about me. 
-    `
-}
